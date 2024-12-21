3768827018b8c00be8becbb52eda36c41f9d4923
Mitso9898 <162876584+Mitso9898@users.noreply.github.com>
2024-12-21 16:00:31 +0200

    refactor app module and main entry to enhance configuration management and integrate TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import validationSchema from './config/validationSchema';
-import configuration from './config/configuration';
-import { ConfigModule } from '@nestjs/config';
+import validationSchema from './config/environmentVariables/validationSchema';
+import configuration from './config/environmentVariables/configuration';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getDatabaseConfig } from './config/db/db.config';
+import { Product } from './entities/product.entity';
 
 @Module({
   imports: [
@@ -12,6 +15,11 @@ import { ConfigModule } from '@nestjs/config';
       validationSchema,
       isGlobal: true,
     }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: getDatabaseConfig,
+    }),
+    TypeOrmModule.forFeature([Product]),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
-import { ConfigurationStructure } from './config/ConfigurationStructure.interface';
+import { CONFIG_KEYS } from './config/environmentVariables/configuration.constants';
+import { ConfigurationStructure } from './config/environmentVariables/configurationStructure.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService =
     app.get<ConfigService<ConfigurationStructure>>(ConfigService);
 
-  const port = configService.get<ConfigurationStructure['PORT']>('PORT');
-  const globalPrefix =
-    configService.get<ConfigurationStructure['GLOBAL_PREFIX']>('GLOBAL_PREFIX');
+  const port = configService.get<ConfigurationStructure['PORT']>(
+    CONFIG_KEYS.PORT,
+  );
+  const globalPrefix = configService.get<
+    ConfigurationStructure['GLOBAL_PREFIX']
+  >(CONFIG_KEYS.GLOBAL_PREFIX);
 
   app.setGlobalPrefix(globalPrefix);
-  
+
   await app.listen(port);
 }
 bootstrap();
