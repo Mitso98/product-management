f41a5d5d59c0b58b2fce6a330d16ce81d9383af3
Mitso9898 <162876584+Mitso9898@users.noreply.github.com>
2024-12-21 20:09:17 +0200

    add throttling and exception handling; configure rate limiting with dynamic settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
+import {
+  ThrottlerGuard,
+  ThrottlerModule,
+} from '@nestjs/throttler';
 import validationSchema from './config/environmentVariables/validationSchema';
 import configuration from './config/environmentVariables/configuration';
 import { getDatabaseConfig } from './config/db/db.config';
 import { LoggerModule } from './logger/logger.module';
 import { RequestLoggerMiddleware } from './logger/request-logger/request-logger.middleware';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { AppConfigInterface } from './config/environmentVariables/configurationInterface.interface';
 
 @Module({
   imports: [
@@ -20,10 +27,33 @@ import { RequestLoggerMiddleware } from './logger/request-logger/request-logger.
       inject: [ConfigService],
       useFactory: getDatabaseConfig,
     }),
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const appEnv = configService.get<AppConfigInterface>('app');
+        return [
+          {
+            ttl: appEnv.RATE_LIMIT_TTL * 1000,
+            limit: appEnv.RATE_LIMIT_MAX,
+          },
+        ];
+      },
+    }),
     LoggerModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
